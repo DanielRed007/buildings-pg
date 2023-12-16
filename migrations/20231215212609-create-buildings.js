37a8bb7a0ec9125e7c28ed0b548b8c6cccfe9359
danielred007 <danielrojo1927@gmail.com>
2023-12-16 07:56:24 +0000

    first commit

---
+++ migrations/20231215212609-create-buildings.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('buildings', {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('buildings');
+  },
+};
+
