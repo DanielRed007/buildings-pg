3b20c42a47d2d1f8a1caa2b9ede70e8af87d5908
danielred007 <danielrojo1927@gmail.com>
2023-12-22 21:29:16 +0000

    basic buildings crud complete

--- src/repositories/buildings.repository.ts
+++ src/repositories/buildings.repository.ts
@@ -8,25 +8,62 @@ interface IBuildingRepository {
 
 class BuildingRepository implements IBuildingRepository {
 
-    async retrieveAll(): Promise<Building[]> {
-        try {
-          return await Building.findAll();
-        } catch (error) {
-          throw error;
-        }
+  async retrieveAll(): Promise<Building[]> {
+    try {
+      return await Building.findAll();
+    } catch (error) {
+      throw error;
     }
-
-    async create(building: Building): Promise<Building> {
-      try {
-        return await Building.create({
-          name: building.name,
-          address: building.address
-        });
-      } catch (error) {
-        throw error;
-      }
   }
 
+  async create(building: Building): Promise<Building> {
+    try {
+      return await Building.create({
+        name: building.name,
+        address: building.address
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async retrieveById(id: string): Promise<Building | null> {
+    try {
+      return await Building.findByPk(id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateById(id: string, updates: Partial<Building>): Promise<Building | null> {
+    try {
+      const building = await Building.findByPk(id);
+
+      if(building){
+        await building.update(updates);
+        return building;
+      }
+
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteById(id: string): Promise<boolean> {
+    try {
+      const building = await Building.findByPk(id);
+
+      if(building){
+        await building.destroy();
+        return true;
+      }
+
+      return false;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default new BuildingRepository();
