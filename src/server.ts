610b060a8accfcaf475266d16a4766d2c551d1b9
danielred007 <danielrojo1927@gmail.com>
2023-12-23 19:01:21 +0000

    swagger UI implemented, buildings resource basic crud included in the documentation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express, { Application } from "express";
 import cors, { CorsOptions } from "cors";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 import Routes from "./routes";
-// import DB from "./db";
 
 export default class Server {
   constructor(app: Application) {
     this.config(app);
     new Routes(app);
-    // new DB();
   }
 
   private config(app: Application): void {
@@ -15,6 +15,16 @@ export default class Server {
       origin: "http://localhost:8081"
     };
 
+    const swaggerDefinition = require('../swagger.json');
+
+    const options = {
+      swaggerDefinition,
+      apis: ['./src/routes/*.ts'], // Path to the API routes files
+    };
+
+    const swaggerSpec = swaggerJSDoc(options);
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     app.use(cors(corsOptions));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
